538b79dcaafdf698256feb1a4c6aa5f75db2cb4d
Eeshakumari <r.eeshakumari03@gmail.com>
2025-10-07 22:32:21 +0530

    about and contact page

---
+++ src/utils/Tech-stack.js
@@ -0,0 +1,55 @@
+import Html from "../assets/icons/html.svg";
+import Css from "../assets/icons/css.svg";
+import Sass from "../assets/icons/sass.svg";
+import JavaScript from "../assets/icons/js.svg";
+import ReactIcon from "../assets/icons/react.svg";
+import MaterialUi from "../assets/icons/material.png";
+import Flutter from "../assets/icons/flutter.svg";
+import Node from "../assets/icons/node.svg";
+import Express from "../assets/icons/expressjs.png";
+import MongoDB from "../assets/icons/mongoDB.svg";
+import Git from "../assets/icons/github.svg";
+import TailwindCSS from "../assets/icons/tailwindcss.svg";
+import TypeScript from "../assets/icons/typescript.svg";
+import NextJs from "../assets/icons/nextjs.svg";
+import Angular from "../assets/icons/angular.svg";
+import Redux from "../assets/icons/redux.svg";
+import MySQL from "../assets/icons/mysql.svg";
+import PostgreSQL from "../assets/icons/postgresql.svg";
+import Jest from "../assets/icons/jest.svg";
+import Cypress from "../assets/icons/cypress.svg";
+import Figma from "../assets/icons/figma.svg";
+import AdobeXD from "../assets/icons/adobexd.svg";
+import Photoshop from "../assets/icons/photoshop.svg";
+import AWS from "../assets/icons/aws.svg";
+import Docker from "../assets/icons/docker.svg";
+import Kubernetes from "../assets/icons/kubernetes.svg";
+
+export const techStack = [
+  { icon: Html, techName: "HTML5", link: "", experience: "04/19/2020", level: "90" },
+  { icon: Css, techName: "CSS3", link: "", experience: "04/19/2020", level: "90" },
+  { icon: Sass, techName: "SASS/SCSS", link: "", experience: "06/19/2021", level: "85" },
+  { icon: JavaScript, techName: "JavaScript (ES6+)", link: "", experience: "04/19/2020", level: "90" },
+  { icon: TypeScript, techName: "TypeScript", link: "", experience: "06/10/2022", level: "85" },
+  { icon: ReactIcon, techName: "React.js", link: "", experience: "06/19/2020", level: "90" },
+  { icon: Angular, techName: "Angular 12–18", link: "", experience: "06/19/2020", level: "85" },
+  { icon: NextJs, techName: "Next.js", link: "", experience: "10/05/2023", level: "80" },
+  { icon: Redux, techName: "Redux / Redux Toolkit", link: "", experience: "06/19/2020", level: "85" },
+  { icon: Node, techName: "Node.js", link: "", experience: "06/10/2022", level: "80" },
+  { icon: Express, techName: "Express.js", link: "", experience: "06/10/2022", level: "75" },
+  { icon: MongoDB, techName: "MongoDB", link: "", experience: "06/10/2022", level: "75" },
+  { icon: MySQL, techName: "MySQL", link: "", experience: "04/10/2021", level: "80" },
+  { icon: PostgreSQL, techName: "PostgreSQL", link: "", experience: "04/10/2021", level: "75" },
+  { icon: Jest, techName: "Jest", link: "", experience: "04/19/2021", level: "80" },
+  { icon: Cypress, techName: "Cypress", link: "", experience: "04/19/2022", level: "80" },
+  { icon: TailwindCSS, techName: "Tailwind CSS", link: "", experience: "07/02/2023", level: "80" },
+  { icon: MaterialUi, techName: "Material UI", link: "", experience: "05/19/2021", level: "85" },
+  { icon: Figma, techName: "Figma", link: "", experience: "05/19/2021", level: "90" },
+  { icon: AdobeXD, techName: "Adobe XD", link: "", experience: "05/19/2021", level: "85" },
+  { icon: Photoshop, techName: "Adobe Photoshop", link: "", experience: "05/19/2020", level: "85" },
+  { icon: AWS, techName: "AWS", link: "", experience: "01/10/2022", level: "75" },
+  { icon: Docker, techName: "Docker", link: "", experience: "01/10/2022", level: "70" },
+  { icon: Kubernetes, techName: "Kubernetes", link: "", experience: "01/10/2022", level: "70" },
+  { icon: Git, techName: "Git / GitHub", link: "", experience: "04/19/2020", level: "80" },
+  { icon: Flutter, techName: "Flutter/Dart", link: "", experience: "01/10/2023", level: "10" }
+];
